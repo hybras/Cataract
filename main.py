26ebc6222616c6bf81a057b653fa1c3a69e240a2
K2S0 <varunchari@yahoo.com>
2019-10-20 02:58:58 -0400

    Remove Comments

--- main.py
+++ main.py
@@ -21,34 +21,14 @@ def extra_processing(pipeline):
     widths = []
     heights = []
 
-    # Find the bounding boxes of the contours to get x, y, width, and height
-    # for contour in pipeline.filter_contours_output:
-    #     x, y, w, h = cv2.boundingRect(contour)
-    #     center_x_positions.append(
-    #         x + w / 2
-    #     )  # X and Y are coordinates of the top-left corner of the bounding box
-    #     center_y_positions.append(y + h / 2)
-    #     widths.append(w)
-    #     heights.append(h)
-    #     print((x, y, w, h))
+
 
     for keypoint in pipeline.find_blobs_output:
-        # x, y = keypoint.pt[0], keypoint.pt[1]
         print(keypoint.pt)
 
-    # Publish to the '/vision/red_areas' network table
-    # table = NetworkTables.getTable('/vision/red_areas')
-    # table.putNumberArray('x', center_x_positions)
-    # table.putNumberArray('y', center_y_positions)
-    # table.putNumberArray('width', widths)
-    # table.putNumberArray('height', heights)
 
 
 def main():
-    # print('Initializing NetworkTables')
-    # NetworkTables.setClientMode()
-    # NetworkTables.setIPAddress('localhost')
-    # NetworkTables.initialize()
 
     print("Creating video capture")
     cap = cv2.VideoCapture(0)
